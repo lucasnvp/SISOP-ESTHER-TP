94a41753e2d0794a726eeb99b4d51502f6aeaf65
ssalvatelli <santiago_salvatelli@hotmail.com>
2017-05-15 17:44:29 -0300

    Agrego hilos a la consola

--- Proceso_Consola/src/Proceso_Consola.c
+++ Proceso_Consola/src/Proceso_Consola.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include "servidor/servidor.h"
 #include "config/config_consola.h"
 #include "utiles/ComandosPorConsola.h"
-#include "serializador/serializador.h"
 
 char* PATH_CONFIG = "../src/config/config.cfg";
 
@@ -31,16 +31,16 @@ int main (void){
 	//Bucle para el ingreso de datos
 	while (!salir) {
 		t_Consola consola = leerComandos();
+		consola.kernel = kernel;
 		if (!strcmp(consola.comando, "run") || !strcmp(consola.comando, "stop")) {
 			if (consola.argumento == NULL)
 				printf("Falta el argumento de la funcion %s\n", consola.comando);
-			else //Envio el mensaje
-				//Serializo el path
-				serializar_path(kernel, 2, strlen(consola.argumento), consola.argumento);
-				//Recibo los datos
-				DatosRecibidos *buffer = deserializar_path(kernel);
-				//Muestro los datos
-				printf("Me llegaron %d bytes con %s\n", buffer->bytesRecibidos, buffer->datos);
+			else {
+				//Envio el mensaje
+				pthread_t hiloConsola;
+				t_Consola parametro = consola;
+				pthread_create(&hiloConsola, NULL, (void*) crearHiloConsola, (void*) &parametro);
+			}
 		}
 		else if (!strcmp(consola.comando, "exit"))
 			salir = 1;
--- Proceso_Consola/src/utiles/ComandosPorConsola.h
+++ Proceso_Consola/src/utiles/ComandosPorConsola.h
@@ -1,13 +1,17 @@
 #ifndef SRC_UTILES_COMANDOSPORCONSOLA_H_
 #define SRC_UTILES_COMANDOSPORCONSOLA_H_
+#include "../serializador/serializador.h"
+#include <stdio.h>
 
 typedef struct {
+	int kernel;
 	char* comando;
 	char* argumento;
 } t_Consola;
 
 void limpiarBufferDeEntrada();
 t_Consola leerComandos();
+void crearHiloConsola(t_Consola* consola);
 
 void limpiarBufferDeEntrada() {
 	char c;
@@ -18,13 +22,29 @@ t_Consola leerComandos() {
 	//Mensaje
 	char* mensaje = (char *) malloc(sizeof(char) * 1000);
 	t_Consola consola;
-	printf("> ");
+	consola.kernel = 0;
+	consola.comando = "";
+	consola.argumento = "";
+	//printf("> ");
 	scanf("%[^\n]s", mensaje);
-	consola.comando = strtok(mensaje, " ");
-	consola.argumento = strtok(NULL, " ");
+	if (strcmp(mensaje, "")) {
+		consola.comando = strtok(mensaje, " ");
+		consola.argumento = strtok(NULL, " ");
+	}
 	limpiarBufferDeEntrada();
 	free(mensaje);
 	return consola;
+}
+
+void crearHiloConsola(t_Consola* consola) {
+	//Serializo el path
+	serializar_path(consola->kernel, 2, strlen(consola->argumento), consola->argumento);
+
+	//Recibo los datos
+	DatosRecibidos *buffer = deserializar_path(consola->kernel);
+
+	//Muestro los datos
+	printf("Me llegaron %d bytes con %s\n", buffer->bytesRecibidos, buffer->datos);
 }
 
 #endif /* SRC_UTILES_COMANDOSPORCONSOLA_H_ */
