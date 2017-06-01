37c485f654c3c1f4d79bed902af14320a8089502
GuidoBeltrame <guidobeltrame@est.frba.utn.edu.ar>
2017-06-01 19:49:55 -0300

    Continuo con armado de funciones para pcb e interprete ansisop

--- Proceso_CPU/src/Proceso CPU.c
+++ Proceso_CPU/src/Proceso CPU.c
@@ -1,88 +1,173 @@
-#include "Proceso CPU.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include "serializador/serializador.h"
 #include "config/config_CPU.h"
 #include "pcb/pcb.h"
+#include "commons/collections/list.h"
+#include "Proceso CPU.h"
+#include <parser/metadata_program.h>
 
 char* PATH_CONFIG = "../src/config/config.cfg";
+
+
+static const char* PROGRAMA =
+						"begin\n"
+						"variables a, b\n"
+						"a = 3\n"
+						"b = 5\n"
+						"a = b + 12\n"
+						"end\n"
+						"\n";
+
+//TODAS LAS PRIMITIVAS TIENEN QUE ESTAR ACA
+/*AnSISOP_funciones functions = {
+	.AnSISOP_definirVariable	= dummy_definirVariable,
+	.AnSISOP_obtenerPosicionVariable= dummy_obtenerPosicionVariable,
+	.AnSISOP_finalizar = dummy_finalizar,
+	.AnSISOP_dereferenciar	= dummy_dereferenciar,
+	.AnSISOP_asignar	= dummy_asignar,
+
+	 .AnSISOP_obtenerValorCompartida
+	 .AnSISOP_asignarValorCompartida
+	 .AnSISOP_irAlLabel
+	 .AnSISOP_llamarSinRetorno
+	 .AnSISOP_llamarConRetorno
+	 .AnSISOP_retornar
+
+};*/
+
+AnSISOP_kernel kernel_functions = {/*
+		.AnSISOP_wait
+		.AnSISOP_signal
+		.AnSISOP_reservar
+		.AnSISOP_liberar
+		.AnSISOP_abrir
+		.AnSISOP_borrar
+		.AnSISOP_cerrar
+		.AnSISOP_moverCursor
+		.AnSISOP_escribir
+		.AnSISOP_leer
+		*/
+
+};//NO SE PARA QUE ES ESTO
+
 
 // ------------------------ VARIABLES GLOBALES ---------------------------------
 
 uint32_t idCpu; // Identificador de CPU.
+uint32_t existePCB = 0;		//Cuando esto incremente a 1 , significa que ya recibio un PCB correcto
+							//si queda en 0 significa que no hay todavia. Cuando la CPU se libere del PCB actual porque
+							//ya realizo todas sus operaciones correspondientes , entonces se vuelve a setear en 0
+
+t_list *listaDePCBs;  //Esta lista va a manejar todos los PCBs activos que representarian a las distintas CPUs que se encuentran procesando.
+
+//Variables para sockets
+
+int kernel;
+int memoria;
+
 
 int main(void) {
 	puts("Proceso CPU");
-
-	idCpu = 1;
 
 	//Cargo archivo de configuracion y muestro
 	abrir_config(PATH_CONFIG);
 	mostrarConfig();
 
 	//Conexion al servidor
-	int kernel = connect_server(ipKernel(), puertoKernel());
+	kernel = connect_server(ipKernel(), puertoKernel());
 
 	//Si conecto, informo
 	if (kernel > 0) {
 		printf("Estoy escuchando\n");
 	}
 
-	char* buff;
-	while (1) {
-		//Recibo los datos
-		recive_data(kernel,buff,sizeof(buff));
-		enviarMensaje("Hola Kernel Serializado",0,1,kernel);
+	listaDePCBs = list_create(); //Instancio la lista de PCBs
+
+	while (existePCB == 0) {
+
+		ejecutarPrograma();
 
 	}
-
-
 
 	cerrar_config_actual();
 
 	return EXIT_SUCCESS;
 }
 
+bool terminoElPrograma(void){
+	return termino;
+}
 
-//Funcion para enviar un mensaje al Kernel con un literal
 
-void enviarMensaje(char* mensaje, int valor, int idMje, int socket) {
+void ejecutarPrograma(){
 
-	t_STR_INTERCAMBIO str_mensaje = incializarIntercambio();
-	str_mensaje.socket_server = socket;
-	str_mensaje.identificador_cpu = idCpu;
-	str_mensaje.size_str = sizeof(t_STR_INTERCAMBIO);
+	recibirPCB(kernel);
 
-	if (idMje == 1) { //IMPRIMIR TEXTO
-		str_mensaje.id_MSJ = IMPRIMIR_TEXTO;
-		serializar_path(str_mensaje.socket_server,str_mensaje.id_MSJ ,str_mensaje.size_str, str_mensaje.texto);
-	} else if (idMje == 2) { //IMPRIMIR VALOR
-		str_mensaje.id_MSJ = IMPRIMIR_VALOR;
-		str_mensaje.valor = valor;
+	char *programa = strdup(PROGRAMA);//copia el programa entero en esa variable
+
+	t_metadata_program *metadata = metadata_desde_literal(programa);//hacerlo por que si
+
+	int programCounter = 0;//deberia ser el del PCB
+
+	while(!terminoElPrograma()){
+
+//		char* const linea = conseguirDatosDeLaMemoria(
+//				programa,
+//				//	metadata->instrucciones_serializado[programCounter].start,
+//				metadata->instrucciones_serializado[programCounter].offset);//que me devuelva la siguiente linea la memoria
+//				printf("\t Evaluando -> %s", linea);
+//			//	analizadorLinea(linea, &functions, &kernel_functions);//que haga lo que tenga q hacer
+//		free(linea);
+//		programCounter++;
 	}
-	serializar_path(str_mensaje.socket_server,1,str_mensaje.size_str, str_mensaje.texto);
-}
 
-t_STR_INTERCAMBIO incializarIntercambio(){
+	metadata_destruir(metadata);//por que si
 
-	t_STR_INTERCAMBIO str;
+//conseguirDatosMemoria(pcb,0,8);
+//connectionHandler(socketKernel);
 
-	str.buffer = "\0";
-	str.codigo = "\0";
-	str.id_var_comp = "\0";
-	str.mensaje = "\0";
-	str.texto = "\0";
-	str.PID = 0;
-	str.OK = 0;
-	str.base_bytes = 0;
-	str.identificador_cpu = 0;
-	str.indice = 0;
-	str.offset_bytes = 0;
-	str.tamanio_bytes = 0;
-	str.valor = 0;
-	return str;
 
 }
+
+//Funcion para esperar un PCB del Kernel
+
+void recibirPCB(int socket){
+
+	DatosRecibidos *pcbRecibido = deserializar_path(socket); //Deserializo la info que trae el socket
+
+	if(pcbRecibido == NULL){  //Si el socket no tiene info valida tiro error.
+
+		printf("%s","Error al recibir PCB desde Kernel");
+
+	}
+
+	if(pcbRecibido != NULL){//Si el socket viene con info valida lo almaceno
+
+		existePCB++; //Incremento el contador de existencia de PCBs
+
+			t_PCB *pcbInterno = malloc(sizeof(t_PCB)); //Pido memoria para inicializar el pcb interno
+
+			//Cargo el PCB interno con los datos que provienen del Deserializador
+
+			pcbInterno->ProgramCounter = 1; //pcbRecibido->ProgramCounter;
+			pcbInterno->StackPointer = 1; // pcbRecibido->stackPointer;
+			pcbInterno->PageCode = 1; // pcbRecibido->PageCode;
+			pcbInterno->TagsPointer = 1; //pcbRecibido->TagsPointer;
+			pcbInterno->ExitCode = 0; // pcbRecibido->ExitCode
+			pcbInterno->CodePointer = 1; //pcbRecibido->CodePointer;
+			list_add(listaDePCBs,pcbInterno); //Agrego el pcb interno a la lista de PCBs activos.
+			free(pcbInterno); //libero memoria solicitada
+
+
+	}
+
+}
+
+
+
+
+
 
 
 
--- Proceso_CPU/src/Proceso CPU.h
+++ Proceso_CPU/src/Proceso CPU.h
@@ -8,9 +8,11 @@
 #define IMPRIMIR_TEXTO 1;
 #define IMPRIMIR_VALOR 2;
 
+bool termino = false;
+
 typedef struct pcb { // Estructura que representa el PCB
 	uint32_t PID; //Número identificador del proceso único en el sistema.
-	uint32_t PC; //Número de la próxima instrucción del Programa que se	debe ejecutar.
+	uint32_t ProgramCounter; //Número de la próxima instrucción del Programa que se	debe ejecutar.
 	uint32_t PageCode; //Cantidad de páginas utilizadas por el código del Programa AnSISOP, empezando por la página cero.
 	uint32_t CodePointer; //Estructura auxiliar que contiene el offset del inicio y del	fin de cada sentencia del Programa.
 	uint32_t TagsPointer; //Estructura auxiliar utilizada para conocer las líneas de código correspondientes al inicio de los procedimientos y a las etiquetas.
@@ -19,59 +21,8 @@ typedef struct pcb { // Estructura que representa el PCB
 
 } t_PCB;
 
-typedef struct srtIntercambio {
-
-	//DATOS DEL SOCKET
-	uint32_t disconnected;
-	uint32_t socket_server;
-	uint32_t size_str;
-
-	//TIPO DE MENSAJE
-	uint32_t id_MSJ;
-
-	//HANDSHAKE_PROGRAMA_PLP y HANDSHAKE_CPU_PCP
-	uint32_t identificador_cpu; //(identificador propio de CPU)
-
-	//HANDSHAKE_PROGRAMA_PLP y ENVIO_COD_INS_ETI
-	uint32_t codigo_long;
-	char* codigo; //Ansisop
-
-	//IMPRIMIR - IMPRIMIR_TEXTO
-	uint32_t texto_long;
-	char* texto;
-
-	//OBTENER_VALOR y GRABAR_VALOR
-	uint32_t id_var_comp_long;
-	char* id_var_comp;
-	int valor;
-
-	//SOLICITAR_BYTES y ENVIAR_BYTES
-	uint32_t  base_bytes;
-	uint32_t  offset_bytes;
-	uint32_t  tamanio_bytes;
-
-	//SOLICITAR_BYTES, ENVIAR_BYTES y SOLICTUD_INSTRUCCION
-	uint32_t buffer_long;
-	char* buffer;
-
-	//EJECUTAR_PROGRAMA
-	t_PCB PCB;
-
-	//HANDSHAKE_PROGRAMA_PLP, HANDSHAKE_CPU_PCP, HANDSHAKE_CPU_UMV, CAMBIO_PROCESO_ACTIVO, FINALIZAR_EJECUCION, DESTRUIR_SEGMENTOS, NOTIFICACION_QUANTUM
-	uint32_t PID;
-
-	//ENTRADA_INDICE
-	uint32_t  indice;
-
-	// Proposito Gral
-	uint32_t OK;
-	uint32_t mensaje_long;
-	char* mensaje;
-
-} t_STR_INTERCAMBIO;
-
-
-
-t_STR_INTERCAMBIO incializarIntercambio();
+bool terminoElPrograma(void);
+void ejecutarPrograma();
+void recibirPCB(int socket);
 
 #endif /* PROCESO_CPU_H_ */
