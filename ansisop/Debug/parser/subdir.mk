e5310ee55bb6d35fa4fced5121c2164d8214b2de
Lucas Visser <lucasnvp@gmail.com>
2017-06-03 01:04:10 -0300

    - Error general

--- ansisop/Debug/parser/subdir.mk
+++ ansisop/Debug/parser/subdir.mk
@@ -20,11 +20,7 @@ C_DEPS += \
 parser/%.o: ../parser/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-<<<<<<< HEAD
 	gcc -I"/home/utnso/Blacklist/tp-2017-1c-Blacklist/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
-=======
-	gcc -I"/home/utnso/Escritorio/tp-2017-1c-Blacklist/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
->>>>>>> 9a978abd1d9edd60fcc5fa4a19cbea4fff1b5713
 	@echo 'Finished building: $<'
 	@echo ' '
 
