fecd1c741266a17e6c2c2430db08ede09469b4db
GuidoBeltrame <guidobeltrame@est.frba.utn.edu.ar>
2017-06-09 19:27:47 -0300

    Agregando Serializador y PCB

--- Proceso_CPU/src/serializador/serializador.h
+++ Proceso_CPU/src/serializador/serializador.h
@@ -7,6 +7,7 @@
 #include <string.h>
 
 #include "../servidor/servidor.h"
+#include "../pcb/pcb.h"
 
 //CREAR PATH
 typedef struct{
@@ -15,10 +16,13 @@ typedef struct{
 
 }__attribute__((packed)) t_SerialString;
 
+void serializar_int(uint32_t socket, uint32_t number);
+uint32_t deserializar_int(uint32_t socket);
+
 void serializar_string(int client, t_SerialString* PATH);
 void deserializar_string(int servidor, t_SerialString* PATH);
 
-void serializar_int(uint32_t socket, uint32_t number);
-uint32_t deserializar_int(uint32_t socket);
+void serializar_pcb(int client, PCB_t* PCB);
+void deserializar_pcb(int servidor, PCB_t* PCB);
 
 #endif /* SERIALIZADOR_H_ */
