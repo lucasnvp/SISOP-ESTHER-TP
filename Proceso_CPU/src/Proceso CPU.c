708c59b6bc0ebe5ade15e783ba7b0d2d5091d4c7
GuidoBeltrame <guidobeltrame@est.frba.utn.edu.ar>
2017-06-09 19:49:37 -0300

    Conectando CPU con Kernel

--- Proceso_CPU/src/Proceso CPU.c
+++ Proceso_CPU/src/Proceso CPU.c
@@ -3,8 +3,6 @@
 int main(void) {
 	puts("Proceso CPU");
 
-	idCpu = 1; //Asigno id a la Cpu. 50 es que no fue asignado su valor
-
 	//Leemos configuracion
 	config = load_config(PATH_CONFIG);
 	print_config(config);
@@ -15,116 +13,58 @@ int main(void) {
 	//Conexion a memoria
 	connect_server_memoria();
 
-	//------------------------
+	while (true) {
 
-	// Realizar handshake con Kernel
-	str_generica_msjs mensajeAEnviar = inicializar_str_msjs();
-	mensajeAEnviar.id_MSJ = HANDSHAKE_CPU_KERNEL;
-	mensajeAEnviar.socket_descriptor = kernel;
-	mensajeAEnviar.socket_descriptor_server = kernel;
-	//enviar serializado el mensaje a Kernel
-	//enviarMjeSinConsola(kernel,mensajeAEnviar.id_MSJ,&mensajeAEnviar);
+		//Quedo a la espera de recibir un PCB del Kernel
+		deserializar_pcb(kernel, pcbActivo);
 
-	//Se chequea el mensaje del kernel
-	//recibir un dato y deserializarlo
-	str_generica_msjs *mensajeRec;	// = recibirMjeSinConsola(kernel);
+		//Proceso de ejecucion de Primitivas Ansisop
+		ejecutar();
 
-	if (mensajeRec != NULL) {
+		//Envio el mensaje al kernel de que finalizo la rafaga correctamente
+		serializar_int(kernel, FIN_CORRECTO);
 
-		if ((mensajeRec->id_MSJ == HANDSHAKE_CPU_KERNEL)
-				&& (mensajeRec->OK == 1)) {
-
-			idCpu = mensajeRec->identificador_cpu; //asigno el id del cpu
-			printf("Se realizo Handshake con Kernel exitosamente");
-
-		} else {
-			printf("Error al realizar Handshake con Kernel");
-			return EXIT_FAILURE;
-		}
+		//Envio a kernel PCB actualizado
+		serializar_pcb(kernel, pcbActivo);
 
 	}
-
-	free(mensajeRec);
-
-	// Recibir mensaje de Memoria
-
-	str_generica_msjs mjeAEnviarMemoria = inicializar_str_msjs();
-	mjeAEnviarMemoria.id_MSJ = HANDSHAKE_CPU_MEMORIA;
-	mjeAEnviarMemoria.socket_descriptor = memoria;
-	mjeAEnviarMemoria.socket_descriptor_server = memoria;
-	mjeAEnviarMemoria.identificador_cpu = idCpu;
-	//enviar serializado el mensaje a Kernel
-	//enviarMjeSinConsola(memoria,mjeAEnviarMemoria.id_MSJ,&mjeMemoria);
-
-	//Se chequea el mensaje de memoria
-	//recibir un dato y deserializarlo
-
-	str_generica_msjs *mensajeRecMemoria; //= recibirMjeSinConsola(memoria);
-
-	if (mensajeRecMemoria != NULL) {
-
-		if ((mensajeRecMemoria->id_MSJ == HANDSHAKE_CPU_MEMORIA)
-				&& (mensajeRecMemoria->OK == 1)) {
-
-			printf("Recepcion de handshake de Memoria de tipo %i por socket %i",
-					mensajeRecMemoria->id_MSJ, memoria);
-
-	} else {
-		printf("Error en handshake UMV: %s", mensajeRecMemoria->mensaje);
-		return EXIT_FAILURE;
-	}
-
-}
-free(mensajeRecMemoria);
-
-while (1) {
-
-	//Quedo a la espera de recibir un PCB
-
-	//deserializar_int(kernel);
-
-	//Recibo el PCB y solicito a Memoria el stack con el codigo
-
-	//Quedo a la espera de que la memoria me devuelva el codigo
 
 	//Con el codigo doy inicio a la funcion ejecutar() para analizar linea por linea
 
-	//Los resultados los devuelvo al Kernel
-
-	//serializar_string(kernel);
+	return EXIT_SUCCESS;
 
 }
-
-return 0;
-
-}
-
 
 void ejecutar() {
 
-t_intructions indice; //tengo que pedirle el indice a memoria
+	//Quedo a la espera de que la memoria me devuelva el codigo
 
-char* sentencia;//tengo que asignarle la sentencia en base a la instruccion.
-analizadorLinea(strdup(sentencia), &functions, &kernel_functions);//ejecucion de primitivas
+	t_intructions indice; //tengo que pedirle el indice a memoria
+
+	printf("Ejecutando en CPU");
+
+	sleep(5);
+
+	char* sentencia; //tengo que asignarle la sentencia en base a la instruccion.
+	analizadorLinea(strdup(sentencia), &functions, &kernel_functions); //ejecucion de primitivas
 
 }
-
 
 void connect_server_kernel() {
 	//Conexion al kernel
 	kernel = connect_server(config.IP_KERNEL, config.PUERTO_KERNEL);
 	if (kernel > 0) {
 		printf("Kernel conectado, Estoy escuchando\n");
-		serializar_int(kernel,2);
+		serializar_int(kernel, HANDSHAKE_CPU_KERNEL);
 	}
 }
 
 void connect_server_memoria() {
 //Conexion a memoria
-memoria = connect_server(config.IP_MEMORIA, config.PUERTO_MEMORIA);
-if (memoria > 0) {
-	printf("Memoria Conectada\n");
-	serializar_int(memoria, 5);
-}
+	memoria = connect_server(config.IP_MEMORIA, config.PUERTO_MEMORIA);
+	if (memoria > 0) {
+		printf("Memoria Conectada\n");
+		serializar_int(memoria, HANDSHAKE_CPU_MEMORIA);
+	}
 }
 
--- Proceso_CPU/src/Proceso CPU.h
+++ Proceso_CPU/src/Proceso CPU.h
@@ -11,6 +11,8 @@
 #include "servidor/servidor.h"
 #include "primitivas/ansi_primit.h"
 #include "config/config_CPU.h"
+#include "serializador/serializador.h"
+#include "pcb/pcb.h"
 
 char* PATH_CONFIG = "../src/config/config.txt";
 Type_Config config;
@@ -19,21 +21,18 @@ uint32_t kernel;
 uint32_t memoria;
 uint32_t idCpu;
 
+PCB_t *pcbActivo;
 
-AnSISOP_funciones functions = {
-		.AnSISOP_definirVariable = definirVariable,
+AnSISOP_funciones functions = { .AnSISOP_definirVariable = definirVariable,
 //		.AnSISOP_imprimirTexto			= imprimirTexto,
 		.AnSISOP_obtenerPosicionVariable = obtenerPosicionVariable,
-		.AnSISOP_dereferenciar = dereferenciar,
-		.AnSISOP_asignar = asignar,
-		//.AnSISOP_imprimir	= imprimir,
+		.AnSISOP_dereferenciar = dereferenciar, .AnSISOP_asignar = asignar,
+//.AnSISOP_imprimir	= imprimir,
 
-};
+		};
 
-AnSISOP_kernel kernel_functions = {
-		.AnSISOP_signal = semaforo_signal,
-		.AnSISOP_wait = semaforo_wait,
-};
+AnSISOP_kernel kernel_functions = { .AnSISOP_signal = semaforo_signal,
+		.AnSISOP_wait = semaforo_wait, };
 
 //TIPOS DE MENSAJES
 #define IMPRIMIR_TEXTO 1;
@@ -45,63 +44,11 @@ uint32_t cpuOcupado;
 uint32_t idCPU;
 
 // USAR ESTOS MENSAJES
-#define HANDSHAKE_PROGRAMA_PLP 1
 #define HANDSHAKE_CPU_KERNEL 2
-#define HANDSHAKE_CPU_MEMORIA 3
+#define FIN_CORRECTO 3
+#define HANDSHAKE_CPU_MEMORIA 5
 
-typedef struct pcb { // Estructura que representa el PCB
-	uint32_t PID; //Número identificador del proceso único en el sistema.
-	uint32_t ProgramCounter; //Número de la próxima instrucción del Programa que se	debe ejecutar.
-	uint32_t PageCode; //Cantidad de páginas utilizadas por el código del Programa AnSISOP, empezando por la página cero.
-	uint32_t CodePointer; //Estructura auxiliar que contiene el offset del inicio y del	fin de cada sentencia del Programa.
-	uint32_t TagsPointer; //Estructura auxiliar utilizada para conocer las líneas de código correspondientes al inicio de los procedimientos y a las etiquetas.
-	uint32_t StackPointer; //Estructura auxiliar encargada de ordenar los valores almacenados en el Stack.
-	uint32_t ExitCode; //Número entero que identifica el motivo de finalización	del proceso.
-
-} t_PCB;
-
-typedef struct strKernelProgramaCPUUMV {
-
-	//Datos para conexion
-	uint32_t id_MSJ;
-	uint32_t disconnected;
-	uint32_t socket_descriptor;
-	uint32_t socket_descriptor_server;
-	uint32_t size_str;
-	uint32_t identificador_cpu; //(identificador propio de CPU)
-	uint32_t codigo_long; //Para uso ansisop
-	char* codigo; //Para uso ansisop
-	t_PCB PCB; //PCB
-	uint32_t PID; //Identificador del proceso
-	uint32_t OK;	// Para paso de Mensajes
-	uint32_t mensaje_long;	// Para paso de Mensajes
-	char* mensaje;	// Para paso de Mensajes
-
-}__attribute__ ((__packed__)) str_generica_msjs;
-
-str_generica_msjs inicializar_str_msjs() {
-	str_generica_msjs nueva;
-
-	nueva.codigo = "\0";
-	nueva.mensaje = "\0";
-	nueva.PID = 0;
-	nueva.OK = 0;
-	nueva.PCB.PID = 0;
-	nueva.PCB.ProgramCounter = 0;
-	nueva.PCB.CodePointer = 0;
-	nueva.PCB.StackPointer = 0;
-	nueva.PCB.TagsPointer = 0;
-	nueva.PCB.PageCode = 0;
-	nueva.PCB.ExitCode = 0;
-	nueva.disconnected = 0;
-	nueva.id_MSJ = 0;
-	nueva.socket_descriptor = 0;
-	nueva.socket_descriptor_server = 0;
-	nueva.identificador_cpu = 0;
-
-	return nueva;
-}
-
+void ejecutar();
 void connect_server_kernel();
 void connect_server_memoria();
 
