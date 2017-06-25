19fde758b1f8577b3b6ea74c5f32a48227fda537
ssalvatelli <santiago_salvatelli@hotmail.com>
2017-06-25 01:46:23 -0300

    La Consola ya lee un archivo completo, lo empaqueta y lo manda al Kernel

--- Proceso_Consola/src/utiles/ComandosPorConsola.h
+++ Proceso_Consola/src/utiles/ComandosPorConsola.h
@@ -6,7 +6,7 @@
 #include <pthread.h>
 
 typedef struct {
-	int kernel;
+	uint32_t kernel;
 	char* comando;
 	char* argumento;
 } t_Consola;
@@ -14,46 +14,92 @@ typedef struct {
 pthread_mutex_t sem_consola;
 
 void limpiarBufferDeEntrada();
-t_Consola leerComandos();
+char* substr(char* cadena, int comienzo, int longitud);
+t_Consola* leerComandos();
 void crearHiloConsola(t_Consola* consola);
 
 void limpiarBufferDeEntrada() {
 	char c;
-	while ((c = getchar()) != '\n' && c != EOF) { }
+	while ((c = getchar()) != '\n' && c != EOF) {
+	}
 }
 
-t_Consola leerComandos() {
+char* substr(char* cadena, int comienzo, int longitud) {
+	char *nuevo = (char*) malloc(sizeof(char) * (longitud + 1));
+	nuevo[longitud] = '\0';
+	strncpy(nuevo, cadena + comienzo, longitud);
+
+	return nuevo;
+}
+
+t_Consola* leerComandos() {
 	//Mensaje
-	char* mensaje = (char *) malloc(sizeof(char) * 1000);
-	t_Consola consola;
-	consola.kernel = 0;
-	consola.comando = "";
-	consola.argumento = "";
+	char* mensaje = (char *) malloc(sizeof(char) * 2000);
+	t_Consola* consola = (t_Consola *) malloc(sizeof(t_Consola));
+	consola->kernel = 0;
+	consola->comando = "";
+	consola->argumento = "";
 	scanf("%[^\n]s", mensaje);
 	if (strcmp(mensaje, "")) {
-		consola.comando = strtok(mensaje, " ");
-		consola.argumento = strtok(NULL, " ");
+		consola->comando = strtok(mensaje, " ");
+		consola->argumento = strtok(NULL, " ");
 	}
 	limpiarBufferDeEntrada();
-	free(mensaje);
+	//free(mensaje);
 	return consola;
 }
 
 void crearHiloConsola(t_Consola* consola) {
 	t_Consola* param = consola;
 
-	//Ejecuto el comando run en el servidor
-	serializar_int(param->kernel,1);
+	if (strcmp(substr(param->argumento, 0, 1), "/")
+			&& strcmp(substr(param->argumento, 0, 1), ".")) {
+		//Archivo en directorio local
+		char* aux = (char *) malloc(
+				sizeof(char) * strlen(param->argumento) + 3);
+		strcpy(aux, "./");
+		strcat(aux, param->argumento);
+		strcpy(param->argumento, aux);
+		free(aux);
+	}
 
-	//Le envio el path
+	FILE *archivo = fopen(param->argumento, "r");
+
+	if (!archivo) {
+		perror("Error");
+		printf("No se pudo abrir el archivo %s\n\n> ", param->argumento);
+		fflush(stdout);
+		pthread_exit(NULL);
+	}
+
+	char* caracteres = (char *) malloc(sizeof(char) * 1000);
+	char* contenido = (char *) malloc(sizeof(char) * 10000);
+
+	while (!feof(archivo)) {
+		strcpy(caracteres, "");
+		fgets(caracteres, 1000, archivo);
+		if (strlen(contenido))
+			strcat(contenido, caracteres);
+		else
+			strcpy(contenido, caracteres);
+	}
+
+	free(caracteres);
+	fclose(archivo);
+
+	//Ejecuto el comando run en el servidor
+	serializar_int(param->kernel, 1);
+
+	//Le envio el archivo completo
 	t_SerialString* path = malloc(sizeof(t_SerialString));
-	path->sizeString = strlen(param->argumento) - 1;
+	path->sizeString = strlen(contenido) - 1;
 	path->dataString = malloc(path->sizeString);
-	path->dataString = param->argumento;
+	path->dataString = contenido;
 	serializar_string(param->kernel, path);
+	free(contenido);
 
 	//Recibo los datos
-	uint32_t PID_PCB = deserializar_int(consola->kernel);
+	uint32_t PID_PCB = deserializar_int(param->kernel);
 
 	//Muestro los datos
 	pthread_mutex_lock(&sem_consola);
