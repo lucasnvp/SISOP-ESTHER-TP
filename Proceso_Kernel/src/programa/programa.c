ab886c323595e519c2249d240fc539b6091d86fb
Lucas Visser <lucasnvp@gmail.com>
2017-07-21 15:53:06 -0300

    KERNEL - Al recibir un nuevo proceso este lo alamacena en el programa y efectua el Proceso de PCB.

--- Proceso_Kernel/src/programa/programa.c
+++ Proceso_Kernel/src/programa/programa.c
@@ -1,9 +1,10 @@
 #include "programa.h"
 
-Program* Program_new(uint32_t ID_Consola, uint32_t PID){
+Program* Program_new(uint32_t ID_Consola, uint32_t PID, char* InfoPath){
 	Program* aProgram = malloc(sizeof(Program));
 	aProgram->ID_Consola = ID_Consola;
 	aProgram->PID = PID;
+	aProgram->InfoPath = strdup(InfoPath);
 	return aProgram;
 }
 
@@ -11,9 +12,14 @@ void Program_free(Program *this){
 	free(this);
 }
 
+void InfoPath_free(Program *this){
+	free(this->InfoPath);
+}
+
 void print_Program(Program auxProgram){
 	printf("El socket de la consola es: %d\n",auxProgram.ID_Consola);
 	printf("El PID asociado es: %d\n",auxProgram.PID);
+	printf("La informacion del path es: %s\n", auxProgram.InfoPath);
 }
 
 Program* Search_Program_By_PID(t_list * LIST_CONSOLAS, uint32_t PID){
--- Proceso_Kernel/src/programa/programa.h
+++ Proceso_Kernel/src/programa/programa.h
@@ -2,17 +2,20 @@
 #define PROGRAMA_H_
 
 #include <inttypes.h>
+#include <string.h>
 
 #include <commons/collections/list.h>
 
 typedef struct ProgramType{
 	uint32_t ID_Consola;
 	uint32_t PID;
+	char* InfoPath;
 } Program;
 
-Program* Program_new(uint32_t ID_Consola, uint32_t PID);
+Program* Program_new(uint32_t ID_Consola, uint32_t PID, char* InfoPath);
 Program* Search_Program_By_PID(t_list * LIST_CONSOLAS, uint32_t PID);
 void Program_free(Program *this);
+void InfoPath_free(Program *this);
 void print_Program(Program auxProgram);
 
 #endif /* PROGRAMA_H_ */
